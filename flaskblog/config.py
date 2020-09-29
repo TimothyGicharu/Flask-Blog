63db313752c7e4331dce2d8208e647e25553d61d
Timothy Munene <munenegicharu@gmail.com>
2020-09-29 12:14:54 +0300

    Create database configurations

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost:5432/flaskblog'
+    SQLALCHEMY_DATABASE_URI = 'postgres://twxygqxsnxhzwi:4fcc327b61b60265a981f0f6331a95dd11db3a9429a641059be2ae57d18d26c4@ec2-54-146-142-58.compute-1.amazonaws.com:5432/d76n6u926b0smg'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/quotes.json'
 

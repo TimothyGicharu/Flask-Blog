471c02fac8a84cb1841b4d1cdb75bdacd86974cb
Timothy Munene <munenegicharu@gmail.com>
2020-10-02 14:10:27 +0300

    Add secret key

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -18,7 +18,3 @@ config_options = {
     'development': DevConfig,
     'production' : ProdConfig
 }
-
-
-# app.config['SECRET_KEY'] = '9425c6cec9f7c12e5f836cf482fcf7b1'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://timothy:index506119056@localhost:5432/flaskblog'
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 export SECRET_KEY='9425c6cec9f7c12e5f836cf482fcf7b1'
 python3 manage.py server
+
